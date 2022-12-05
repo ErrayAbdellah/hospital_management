53a5b3c68d43382265676175f684271eb54df8a0
nadir inab <nadir.inab.dev@gmail.com>
2022-12-05 14:28:11 +0100

    Database is added and welcome file is well done

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <?php  
         require "./templates/header.php" ;
-        require "./includes/autoloader.php" ;
-        require "./includes/functions.php" ;
-        $connection = new DbConnection() ;
-        $connect = $connection->connection() ;
+        require "./services/test.php" ;
 
-    $query = "SELECT * FROM dummy" ;
-    $stmt = $connect->query($query) ;
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC) ; 
-    throwData($data[0]) ;
-    // foreach($data[0] as $key => $value){
-    //     echo "$key : $value"  ;
-    //     echo "<hr>" ;
-    // }
-    // die() ;
-    // throwData($data) ;
-?>      
-<div class="container bg-muted text-white">
+        // if(isset($_GET['action']) && $_GET['action'] === 'fetch'){
+        //     $data = fetchdata() ;
+        //     // throwData($data) ;
+        // }
+
+?>    
+ <!-- <div class="container bg-muted text-white">
     <ul class="list-group">
     <?php foreach($data[0] as $key => $value) :   ?>
         <li class="list-group-item"><?= $key ?> <strong><?= $value ?></strong></li>
     <?php endforeach ; ?>
     </ul>
-
 </div>
+<button class="btn btn-muted fw-bold text-white">
+   <a href="index.php?&action=fetch">Fetch</a>
+</button>  -->
+<?php require "./templates/welcome.php" ; ?>
+
 <?php  require "./templates/footer.php" ; ?>
     
 
