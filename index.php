050e926966e395c7e9e39729c3f628f3870f9d34
nadir inab <nadir.inab.dev@gmail.com>
2022-12-05 16:35:49 +0100

    welcome page is fine and good looking

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
         require "./templates/header.php" ;
         require "./services/test.php" ;
 
-        if(isset($_GET['action']) && $_GET['action'] === 'fetch'){
-            $data = fetchdata() ;
-            // throwData($data) ;
-        }
+        // if(isset($_GET['action']) && $_GET['action'] === 'fetch'){
+        //     $data = fetchdata() ;
+        //     // throwData($data) ;
+        // }
 
 ?>    
- <div class="container bg-muted text-white">
+ <!-- <div class="container bg-muted text-white">
     <ul class="list-group">
     <?php foreach($data[0] as $key => $value) :   ?>
         <li class="list-group-item"><?= $key ?> <strong><?= $value ?></strong></li>
@@ -17,7 +17,7 @@
 </div>
 <button class="btn btn-muted fw-bold text-white">
    <a href="index.php?&action=fetch">Fetch</a>
-</button> 
+</button>  -->
 <?php require "./templates/welcome.php" ; ?>
 
 <?php  require "./templates/footer.php" ; ?>
