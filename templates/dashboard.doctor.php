25a83cc0452e5ccba27b20ce1b0e239bcd71732d
ErrayAbdellah <mahdiabdllah121@gmail.com>
2022-12-05 16:43:28 +0100

    doctor dashboard is added

---
+++ templates/dashboard.doctor.php
@@ -0,0 +1,16 @@
+<?php require 'header.php' ?>
+
+<nav class="nav d-flex justify-content-between m-3">
+    <h4 class="d-flex align-items-center">Doctor</h4>
+    <div class="d-flex ">
+       <div>
+       <span class="text-secondary" style="font-size:12px;">Today's Date </span>
+        <h6>2022-12-05 </h6>
+       </div>
+       <i class="far fa-calendar-alt d-flex align-items-center ms-2"  style="font-size: 2rem;"></i>
+    </div>
+</nav>
+<div>
+</div>
+
+<?php require 'footer.php' ?>
