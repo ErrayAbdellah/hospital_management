4212fa110d6f1ab75a1be3003b4c16f212259734
ErrayAbdellah <mahdiabdllah121@gmail.com>
2022-12-05 17:22:10 +0100

    edit Doctor dashboard and added css style

--- templates/dashboard.doctor.php
+++ templates/dashboard.doctor.php
@@ -1,4 +1,15 @@
-<?php require 'header.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Hospital Management</title>
+</head>
+<body>
 
 <nav class="nav d-flex justify-content-between m-3">
     <h4 class="d-flex align-items-center">Doctor</h4>
@@ -10,7 +21,11 @@
        <i class="far fa-calendar-alt d-flex align-items-center ms-2"  style="font-size: 2rem;"></i>
     </div>
 </nav>
-<div>
+<div class=" rounded-3" id="bg_img_dashboard_dctr">
+    <h6>Welcome !</h6>
+    <h5>Test Doctor</h5>
+    <p>Thanks for joinnig with us. We are always trying to get you a complete service
+        You can view your dailly schedule, Reach Patients Appointment at home!</p>
 </div>
 
 <?php require 'footer.php' ?>
