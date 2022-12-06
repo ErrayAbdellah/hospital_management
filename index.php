101bb1cfc86af64e4a461ab10066c827f67c5154
nadir inab <nadir.inab.dev@gmail.com>
2022-12-06 15:17:46 +0100

    classes has been added and and sign Up and signIn style is cool and done

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php  
         require "./templates/header.php" ;
-        require "./services/test.php" ;
+        require "./includes/functions.php" ;
+        if(isset($_POST["signUp"])){
+                
+        }
 
-        // require "./templates/SignIn.php" ;
-        require "./templates/SignUp.php" ;
+        if(isset($_POST["signIn"])){
+                throwData($_POST) ;
+        }
+        require "./services/test.php" ;
+        require "./templates/SignIn.php" ;
 ?>    
 
 
- <!-- <?php require "./templates/welcome.php" ; ?> -->
+
 
 <?php  require "./templates/footer.php" ; ?>
-    
 
 
