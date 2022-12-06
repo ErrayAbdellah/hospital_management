f2919d34ea04c72b176c25d078c7647a1ba3c511
nadir inab <nadir.inab.dev@gmail.com>
2022-12-06 17:40:52 +0100

    code mustb be cleaned

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php  
         require "./templates/header.php" ;
+        require "./includes/autoloader.php" ;
+        
         require "./includes/functions.php" ;
         require "./services/userCrud.php" ;
         if(isset($_POST["signUp"])){
-                
+                echo "hi" ;
         }
         if(isset($_POST["signIn"])){
                 CreateUser() ;
         }
         
-        require "./templates/SignIn.php" ;
+        require "./templates/sidebar.php" ;
+        $oussama = new Admin ;
+        echo "<hr>" ;
+        echo "here" ;
+        throwData($oussama) ;
 ?>    
+
 
 
 
