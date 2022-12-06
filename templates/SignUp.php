10d7af4f077f1e10ddfb828a636eefd4566b610a
nadir inab <nadir.inab.dev@gmail.com>
2022-12-06 14:06:46 +0100

    sign in and sign up forms are added

---
+++ templates/SignUp.php
@@ -0,0 +1,45 @@
+
+<div id="signUpForm" class="container bg-muted text-dark w-50 p-5 d-block mx-auto ">
+    
+    <h2 class="text-center fw-bold">Let's get Started !</h2>
+    <!-- <span class="text-muted mx-auto d-block w-100">Login with your details to continue !</span> -->
+    <div class="form-group">
+        <label for="name" class="text-dark fw-bold p-3 text-large">Full Name :</label>
+        <input name="fullName" type="text" class="form-control" placeholder="Enter Name">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="adresse"  class="text-dark fw-bold p-3">Adresse :</label>
+        <input name="adresse" type="text" class="form-control"  placeholder="Enter Adresse">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="email"  class="text-dark fw-bold p-3">Email :</label>
+        <input name="email" type="email" class="form-control"  placeholder="Enter Email">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="CIN"  class="text-dark fw-bold p-3">CIN :</label>
+        <input name="CIN" type="text" class="form-control"  placeholder="Enter CIN">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="Birth Day"  class="text-dark fw-bold p-3">Date Of Birth :</label>
+        <input name="birthDate" type="date" class="form-control"  placeholder="Enter Birth day">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="Pwd"  class="text-dark fw-bold p-3">Password :</label>
+        <input name="pwd" type="text" class="form-control"  placeholder="Password">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="ConfirmedPwd"  class="text-dark fw-bold p-3">Confirm Password :</label>
+        <input name="cPwd" type="password" class="form-control"  placeholder="confimed Password">
+        <small class="form-text text-muted"></small>
+    </div>
+    <div class="p-2 mt-3 ">
+        <button name="signUp" class="btn sign ">Sign Up</button>
+        <span class="text-muted"> already Have an account<a href="">   Sign In </a>  </span>
+    </div>
+</div>
