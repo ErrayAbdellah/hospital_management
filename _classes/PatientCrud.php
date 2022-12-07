83d76358e9d05915633e0a9e4c3bbdac6c350922
nadir inab <nadir.inab.dev@gmail.com>
2022-12-07 16:21:19 +0100

    patient is being tested and well added

--- _classes/PatientCrud.php
+++ _classes/PatientCrud.php
@@ -3,12 +3,8 @@
     class PatientCrud {
 
         public static function createPatient($data, $connect){
-            $query = "INSERT INTO patients(fullName, adresse, birthDate, email , CIN, pwd) VALUES(:fullName, :adresse, :email,:CIN, :birthDate, :pwd)";
+            $query = "INSERT INTO patients(fullName, adresse, birthDate, email , CIN, pwd) VALUES(:fullName, :adresse, :birthDate, :email, :CIN, :pwd)";
             $stmt = $connect->prepare($query) ;
             $stmt->execute([":fullName" => $data["fullName"], ":adresse" => $data["adresse"], ":email" => $data["email"] , ":CIN" => $data["CIN"], ":birthDate" => $data["birthDate"], ":pwd" => $data["pwd"]]);
         }
-            //  throwData($data) ;
-            // $query = "INSERT INYO patient(fullName, adresse, birthDate, email , CIN, pwd) VALUES( :fullName, :adresse, :birthDate, :email, :CIN, :pwd)";
-            // $stmt = $connect->prepare($query) ;
-            // $stmt->excute(["fullName" => $data->fullName, "adresse" => $data->adresse, "birthDate" => $data->birthDate, "email" => $data->birthDate, "CIN" => $data["CIN"], "pwd", $data["pwd"]]) ;
-        }
+    }
--- services/patientServices.php
+++ services/patientServices.php
@@ -1,38 +1,20 @@
 <?php
-    // require "./includes/autoloader.php" ;
-    // require "./includes/functions.php" ;
+
     $connection = new DbConnection() ;
     $connect = $connection->connection() ;
-
-    // function fetchdata(){
-    //     global $connect ;
-    //     $query = "SELECT * FROM dummy" ;
-    //     $stmt = $connect->query($query) ;
-    //     $data = $stmt->fetchAll(PDO::FETCH_ASSOC) ;
-    //     return $data ;
-    // } 
 
     function createUser(){
         global $connect ;
         $data = ["fullName" => $_POST["fullName"], "adresse" => $_POST["adresse"], "email" => $_POST["email"] , "CIN" => $_POST["CIN"], "birthDate" => $_POST["birthDate"], "pwd" => $_POST["pwd"]] ;
-        PatientCrud::createPatient($data, $connect) ;
-
-        // $data = ["fullName" => $_POST["fullName"], "adresse" => $_POST["adresse"], "email" => $_POST["email"] , "CIN" => $_POST["CIN"], "birthDate" => $_POST["birthDate"], "pwd" => $_POST["pwd"]] ;
-        // $nadir = new Patient(...$data) ;
-        // $query = "SELECT * FROM patients WHERE email = :email and fullName = :fullName " ;
-        // $stmt = $connect->prepare($query) ;
-        // $stmt->execute(["email" => $data["email"], "fullName" => $data["fullName"]]) ;
-        // $stmt->fetchAll(PDO::FETCH_ASSOC) ;
-        //     if($stmt->rowCount() != 0){ 
-        //         $patientExist = "Exist" ;  
-        //         return $patientExist ;
-        //     }else{
-        //         PatientCrud::createPatient($connect, $data) ;
-        //         return "Created" ;
-        //     }  
-        // throwData($data) ;
-            // echo "<hr>" ;
-        // throwData($nadir) ;
-        // PatientCrud::createPatient($nadir, $connect) ;
+        $query = "SELECT * FROM patients WHERE email = :email and fullName = :fullName" ;
+        $stmt = $connect->prepare($query) ;
+        $stmt->execute([":email" => $data["email"], ":fullName" => $data["fullName"]]) ;
+        $stmt->fetchAll(PDO::FETCH_ASSOC) ;
+        if($stmt->rowCount() != 0){
+            return "Patient already exist" ;
+        }else{
+            PatientCrud::createPatient($data, $connect) ;
+            return "Patient created successfully" ;
+        }
     }
   
