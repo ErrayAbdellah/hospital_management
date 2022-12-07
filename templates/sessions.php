08f11e5eb8db336f2734cb12ec395efa73e16911
nouhailamadrani1 <nouhailamadrani2022@gmail.com>
2022-12-07 14:40:15 +0100

    session appointments header

--- templates/sessions.php
+++ templates/sessions.php
@@ -1,18 +1,9 @@
 
-<?php require 'sidebar.php' ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/bootstrap.min.css">
-    <link rel="stylesheet" href="../style/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Hospital Management</title>
-</head>
-<body>
+<?php require 'sidebar.php';
+      require 'header.php';
+?>
+
+
 <section class="container col-9 ">
     <div class="d-flex justify-content-between p-3">
 <div class="d-flex ">
@@ -21,7 +12,8 @@
 <div class="d-flex gap-2">
             <span><small class="text-muted">Today's Date</small><br><b style="color: #34AEAD;">2022-12-01</b></span>
             <div class="border rounded d-flex justify-content-center align-items-center" style="width: 42px; height: 42px;">
-                <img src="../img/icons/calendar.svg" alt="calender">
+                <!-- <img src="../img/icons/calendar.svg" alt="calender"> -->
+                <i class="fa fa-calendar" style="font-size:30px; "></i>
             </div>
         </div>
 </div>
@@ -30,7 +22,7 @@
 <span class="input-group-text d-flex justify-content-center w-25  " id="basic-addon1">Date :</span>
   <input type="text" class="form-control bg-white" placeholder="jj/mm/aaaa" aria-label="Recipient's username" aria-describedby="basic-addon2 " >
 
-  <span class="  input-group-text w-25 d-flex justify-content-center " id="basic-addon2"  style="background: #34AEAD; color:azure"> <i class="fa fa-filter "></i> Filter</span>
+  <span class=" btn input-group-text w-25 d-flex justify-content-center " id="basic-addon2"  style="background: #34AEAD; color:azure"> <i class="fa fa-filter "></i> Filter</span>
 </div>
 <div class="p-3">
 <table class="table p-3 " style="border-color: #34AEAD ;">
