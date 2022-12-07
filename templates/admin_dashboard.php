03121c77a0211bd2c982e60567fbb4171bef8de1
issam <issammez44@gmail.com>
2022-12-07 14:51:13 +0100

    adding the status in admin dashboard

--- templates/admin_dashboard.php
+++ templates/admin_dashboard.php
@@ -13,6 +13,33 @@
             </div>
         </div>
     </div>
+    <h2 class="fw-semibold my-4">Status</h2>
+    <div class="d-flex gap-3">
+        <div class="d-flex gap-2 col rounded p-2 justify-content-between" style="border: 1px solid #34AEAD;">
+            <span> <b style="color: #34AEAD;">1</b><br><small class="text-muted">Doctors</small></span>
+            <div class="border rounded d-flex justify-content-center align-items-center" style="width: 42px; height: 42px; background: #D9D9D9;">
+                <img src="../img/icons/doctors.svg" alt="doctor">
+            </div>
+        </div>
+        <div class="d-flex gap-2 col rounded p-2 justify-content-between" style="border: 1px solid #34AEAD;">
+            <span> <b style="color: #34AEAD;">3</b><br><small class="text-muted">Patients</small></span>
+            <div class="border rounded d-flex justify-content-center align-items-center" style="width: 42px; height: 42px; background: #D9D9D9;">
+                <img src="../img/icons/patients.svg" alt="patients">
+            </div>
+        </div>
+        <div class="d-flex gap-2 col rounded p-2 justify-content-between" style="border: 1px solid #34AEAD;">
+            <span> <b style="color: #34AEAD;">0</b><br><small class="text-muted">newBooking</small></span>
+            <div class="border rounded d-flex justify-content-center align-items-center" style="width: 42px; height: 42px; background: #D9D9D9;">
+                <img src="../img/icons/book.svg" alt="book">
+            </div>
+        </div>
+        <div class="d-flex gap-2 col rounded p-2 justify-content-between" style="border: 1px solid #34AEAD;">
+            <span> <b style="color: #34AEAD;">0</b><br><small class="text-muted">Today sessions</small></span>
+            <div class="border rounded d-flex justify-content-center align-items-center" style="width: 42px; height: 42px; background: #D9D9D9;">
+                <img src="../img/icons/session.svg" alt="session">
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php require "footer.php" ; ?>
