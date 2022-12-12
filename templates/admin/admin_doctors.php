efad220f7d378310394a1581a8d3d93a68d7f5ae
nouhailamadrani1 <nouhailamadrani2022@gmail.com>
2022-12-12 10:26:31 +0100

    form add doctor

--- templates/admin/admin_doctors.php
+++ templates/admin/admin_doctors.php
@@ -28,7 +28,7 @@
 </div>
 <div class=" p-3 d-flex justify-content-between">
 <h4 class="">Add New Doctor</h4>
-<button type="button" class="btn btn"
+<button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn"
         style="background: #34AEAD; color:azure">
         + Add New
 
@@ -96,7 +96,43 @@
 </table>
 </div>
 </section>
+<form action=""><div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Doctor</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Full name :</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Specialties :</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">E-mail :</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Password :</label>
+            <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+
+          </div>
+          
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn" style="background: #34AEAD; color:azure">Save</button>
+      </div>
+    </div>
+  </div>
+</div></form>
 
 
 
