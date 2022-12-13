e4c5ee8aea2b631bf11218a5b2609c366b711701
issam <issammez44@gmail.com>
2022-12-13 17:14:18 +0100

    add utf8

--- _classes/DbConnection.php
+++ _classes/DbConnection.php
@@ -11,7 +11,7 @@
         public function connection() {
             try{
                 $dsn = "mysql:host=$this->host;dbname=$this->dbName;" ;
-                $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC] ;
+                $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO:: MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'] ;
                 $conn = new PDO($dsn, $this->user, $this->pwd, $options) ;
                 return $conn ;
             }catch(PDOException $e ){
