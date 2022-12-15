a0951e7bdbb4198b5876658364fc1144be576fc1
ErrayAbdellah <mahdiabdllah121@gmail.com>
2022-12-15 12:15:12 +0100

    db is added

--- _classes/User.php
+++ _classes/User.php
@@ -10,37 +10,49 @@
             $this->email = $email ;
             $this->pwd = $pwd ; 
         }
+        
 
         public function getFullName(){
             return $this->fullName ;
         }
-        public function setFullName($fullName){
-            $this->fullName = $fullName;
-        }
+       
         public function getEmail(){
             return $this->email ;
         }
-        public function setEmail($email){
-            $this->email = $email;
-        }
+        
         public function getPwd(){
             return $this->pwd ;
         }
-        public function FullPwd($pwd){
-            $this->pwd = $pwd;
+        public static function signIn($data)
+        {
+            global $connect ;
+            $qry = "SELECT * FROM users where email like :email and psw like :pwd role = admin";
+            $stmt = $connect->prepare($qry) ;
+            $stmt->execute([":email"=>$data['email'],":pwd"=>$data['password']]);
+            $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if($stmt->rowCount() != 0)
+            {
+                header("location:templates/doctor/doctor_dashboard.php") ;
+            }else 
+            {
+                $qry = "SELECT * FROM users where email like :email and psw like :pwd ";
+                $stmt = $connect->prepare($qry) ;
+                $stmt->execute([":email"=>$data['email'],":pwd"=>$data['password']]);
+                $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if($stmt->rowCount() != 0)
+                {
+                    header("location:templates/doctor/doctor_dashboard.php") ;
+                }else 
+                {
+                    $qry = "SELECT * FROM users where email like :email and psw like :pwd ";
+                    $stmt = $connect->prepare($qry) ;
+                    $stmt->execute([":email"=>$data['email'],":pwd"=>$data['password']]);
+                    $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    
+                }
+            }
         }
-        // public function signOut(){
-        //     session_destroy() ;
-        // }
-        
-        // public function signIn($email,$psw){
-        //     try{
-                
-        //     }catch()
-        //     {
-
-        //     }
-        // }
+       
         public function logOut(){
             if(!isset($_SESSION['']))
             {
@@ -49,3 +61,4 @@
         }
 
     }
+    
