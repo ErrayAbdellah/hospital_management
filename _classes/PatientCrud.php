735e85b38a4a7d7271712cd7fa2491bd45c982ee
nouhailamadrani1 <nouhailamadrani2022@gmail.com>
2022-12-15 08:56:46 +0100

    patient

--- _classes/PatientCrud.php
+++ _classes/PatientCrud.php
@@ -7,4 +7,13 @@
             $stmt = $connect->prepare($query) ;
             $stmt->execute([":fullName" => $data["fullName"], ":adresse" => $data["adresse"], ":email" => $data["email"] , ":CIN" => $data["CIN"], ":birthDate" => $data["birthDate"], ":pwd" => $data["pwd"]]);
         }
+        public function makeAppointment(){
+
+        }
+        public function updateAccount(){
+
+        }
+        public function deleteAccount(){
+
+        }
     }
