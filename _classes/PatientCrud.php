874e8bf8c03a8d93d0cef28c951782b1d8f984d0
nouhailamadrani1 <nouhailamadrani2022@gmail.com>
2022-12-15 12:26:05 +0100

    modified

--- _classes/PatientCrud.php
+++ _classes/PatientCrud.php
@@ -11,9 +11,24 @@
 
         }
         public function updateAccount(){
+            $connection = new DbConnection();
+            $connect = $connection->connection();
+
+            $id = $_POST['id'];
+            $fullName = $_POST['fullName'];
+            $adresse = $_POST['adresse'];
+            $birthDate = $_POST['birthDate'];
+            $email = $_POST['email'];
+            $cin = $_POST['CIN'];
+            
+            $pwd = $_POST['pwd'];
+            $query="UPDATE patients SET fullName='$fullName',adresse='$adresse',birthDate='$birthDate',email='$email',CIN='$cin',pwd='$pwd' where id = '$id' ";
+            $stmt = $connect->prepare($query);
+            $stmt->execute();
 
         }
         public function deleteAccount(){
 
         }
+        
     }
--- templates/patients/patient.settings.php
+++ templates/patients/patient.settings.php
@@ -27,7 +27,7 @@
       </div>
       <div class=" ">
     <h4 class="card-title " style="color: #34AEAD;">Account Settings</h4>
-    <h6 class="card-title" >Edit your Account Details & Change Password</h6>
+    <h6 class="card-title " data-bs-toggle="modal" data-bs-target="#exampleModal1" >Edit your Account Details & Change Password</h6>
     </div>
   </div>
 </div>
@@ -57,6 +57,48 @@
 
   </div>
 </section>
+<form action="admin_doctors.php" method="POST" ><div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">updateAccount</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <input type="hidden" id="doctorId" name="doctorId" value="">
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Full name :</label>
+            <input type="text" class="form-control" name="fullName"  value="">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">adresse :</label>
+            <input type="text" class="form-control" name="adresse"  value="">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Email :</label>
+            <input type="email" class="form-control" aria-describedby="emailHelp" name="email" id="doctorEmail" value="">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">CIN :</label>
+            <input type="password" class="form-control"  name="CIN" >
+          </div><div class="mb-3">
+            <label for="recipient-name" class="col-form-label">birthDate :</label>
+            <input type="password" class="form-control"  name="birthDate" >
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Password :</label>
+            <input type="password" class="form-control"  name="pwd" >
+          </div>
+
+          
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn" style="background: #34AEAD; color:azure" name="updateDoctor">Update</button>
+      </div>
+    </div>
+  </div>
+</div></form>
 
 
 
